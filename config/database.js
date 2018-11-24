dc51d69c202b0487260aeef85e758fa068d2f48c
IMMANUEL\Immanuel <immanuelbt@gmail.com>
2018-11-24 19:58:35 +0700

    Refresh

---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  foPool: {
+    user: process.env.FO_USER,
+    password: process.env.FO_PASSWORD,
+    connectString: process.env.FO_CONNECTIONSTRING,
+    poolMin: 10,
+    poolMax: 10,
+    poolIncrement: 0
+  }
+};
